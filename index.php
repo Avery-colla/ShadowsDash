7642a143678f8a7894f6dfb680ae18861f3a92c4
Avery-colla <109880673+Avery-colla@users.noreply.github.com>
2022-08-22 09:08:03 +1000

    Update index.php

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ foreach($servers_in_queue as $server) {
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0 text-white">RAM usage</h5>
+                      <h5 class="card-title text-uppercase text-muted mb-0 text-white">RAM (MEM) USAGE</h5>
                       <span class="h2 font-weight-bold mb-0 text-white"><?= $usedRam . "MB/" . $userdb["memory"] ?>MB</span>
                       <?php
                       $percentage = percentage($usedRam, $userdb["memory"], 100);
